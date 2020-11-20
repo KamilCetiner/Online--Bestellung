86421a25d505b8afb287d87b805cee6f6f1b6f3b
KamilCetiner <cetiner.kamil84@gmail.com>
2020-11-20 21:22:09 +0100

    navbar and container created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Hero from './components/Hero'
+import {BrowserRouter as Router} from 'react-router-dom'
+import {GlobalStyle} from './globalStyles'
+
+
+function App() {
+  return (
+    <Router>
+      <GlobalStyle/>
+  
+     <Hero/>
+    </Router>
+  );
+}
+
+export default App;
